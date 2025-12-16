4623f636c44ffde0a53a714408da0e15765252f2
tinilinsk <tema.ryabkov@gmail.com>
2025-12-16 13:30:45 +0100

    Dodaj pliki projektów.

---
+++ EasyHome/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyHome
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EasyHome/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EasyHome
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
